0ccf3f878c1d18ab0dd7687b87c385f5c520391d
IlyaZholobov <ilyazh47@gmail.com>
2021-08-08 15:11:02 +0300

    commit

---
+++ app/Http/Controllers/Load.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Post;
+use App\Models\Comment;
+
+class Load extends Controller
+{
+    function get()
+    {
+        $posts = Http::get("https://jsonplaceholder.typicode.com/posts")->json();
+        $comments = Http::get("https://jsonplaceholder.typicode.com/comments")->json();
+        
+        foreach ($posts as $_post){
+            $post = new Post();
+            $post->user_id = (int) $_post["userId"];
+            $post->title = $_post["title"] ?? "title";
+            $post->body = $_post["body"] ?? "body";
+            $post->save();
+        }
+
+        foreach ($comments as $_comment){
+            $comment = new Comment();
+            $comment->post_id = (int) $_comment["postId"];
+            $comment->email = $_comment["email"] ?? "email";
+            $comment->name = $_comment["name"] ?? "name";
+            $comment->body = $_comment["body"] ?? "body";
+            $comment->save();
+        }
+
+        return redirect()->route("/random_post");
+    }
+
+    function getPost(){
+        $post = Post::inRandomOrder()->first();
+        $comments =$post->comments;
+        
+        return ["post" => $post, "comments" => $comments];
+    }
+}
